800a75e0afd46ce7be2b5a62a37f7eb64983237c
Juan Daniel Ramírez Mojica <76570410+JuanDanielRamirezMojica@users.noreply.github.com>
2026-03-06 21:19:46 -0500

    feat(forum-service): FastAPI setup + MongoDB connection

---
+++ forum-service/app/main.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import get_settings
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = get_settings()
+    client = AsyncIOMotorClient(settings.mongo_url)
+    app.state.db = client[settings.mongo_db]
+    yield
+    client.close()
+
+def get_app() -> FastAPI:
+    settings = get_settings()
+    app = FastAPI(
+        title='Forum service!',
+        lifespan=lifespan,
+        docs_url='/docs' if settings.debug else None
+    )
+    # ------------- Routers -------------
+    return app
+
+app = get_app()
