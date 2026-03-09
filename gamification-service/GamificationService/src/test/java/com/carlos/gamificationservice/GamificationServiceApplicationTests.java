933b478da5edbe9882f26ac8696c145bf0793ba4
Carlos Ivan <93873199+camunozv@users.noreply.github.com>
2026-03-08 21:30:57 -0500

    chore: gamification service initial commit

---
+++ gamification-service/GamificationService/src/test/java/com/carlos/gamificationservice/GamificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.gamificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
